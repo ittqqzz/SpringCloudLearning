c4e7f17999eae96f58679646d76a861cf415d088
tqz <572393128@qq.com>
2019-05-09 16:08:17 +0800

    eureka 集群搭建成功

---
+++ microservice-eureka-7002/src/main/java/com/tqz/cloud/EurekaServer7002_App.java
@@ -0,0 +1,14 @@
+package com.tqz.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// 启用 eureka 服务端
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer7002_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+}
--- microservice-provider-dept-8001/src/main/java/com/tqz/cloud/DeptProvider8001_App.java
+++ microservice-provider-dept-8001/src/main/java/com/tqz/cloud/DeptProvider8001_App.java
@@ -2,7 +2,10 @@ package com.tqz.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+// 开启 eureka client 端注解，将本 provider 服务注册进  eureka 中
+@EnableEurekaClient
 @SpringBootApplication
 public class DeptProvider8001_App {
 
