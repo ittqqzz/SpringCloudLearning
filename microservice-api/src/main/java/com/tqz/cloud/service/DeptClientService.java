d582d1cfc3008eb189c23c2080a799916720a5ca
tqz <572393128@qq.com>
2019-05-09 10:23:14 +0800

    init project

---
+++ microservice-api/src/main/java/com/tqz/cloud/service/DeptClientService.java
@@ -0,0 +1,4 @@
+package com.tqz.cloud.service;
+
+public interface DeptClientService {
+}
---
+++ microservice-provider-dept-8001/src/main/java/com/tqz/cloud/dao/Dept8001Dao.java
@@ -0,0 +1,15 @@
+package com.tqz.cloud.dao;
+
+import com.tqz.cloud.entities.Dept;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface Dept8001Dao {
+    boolean addDept(Dept dept);
+
+    Dept findById(Long id);
+
+    List<Dept> findAll();
+}
