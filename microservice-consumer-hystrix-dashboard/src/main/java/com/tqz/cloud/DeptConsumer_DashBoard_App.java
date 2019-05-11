fe226028507c2088587eb71665d2c4375b0424ed
tqz <572393128@qq.com>
2019-05-11 15:54:06 +0800

    监控面板搭建成功

---
+++ microservice-consumer-hystrix-dashboard/src/main/java/com/tqz/cloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,13 @@
+package com.tqz.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard // 开启监控面板
+public class DeptConsumer_DashBoard_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class, args);
+    }
+}
